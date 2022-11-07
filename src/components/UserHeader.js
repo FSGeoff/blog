ed3fcc6abe69edd4e10ed8b0b124af5b1101a817
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-11-07 08:44:40 -0500

    styled and finished app

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
+
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
-
     render() {
         const {user} = this.props;
         if(!user){
@@ -21,5 +17,5 @@ const mapToStateProps = (state, ownProps) => {
 
 export default connect(
     mapToStateProps,
-    { fetchUser }
+
 )(UserHeader);
